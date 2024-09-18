a8ac887ce979093e52588564a18f615ec9d143d4
Sandra Borowik <sandra.borowik@vp.pl>
2024-09-18 12:09:50 +0200

    TagHelpers

---
+++ TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,52 @@
+﻿using AquaGuide2.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AquaGuide2.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperfactory)
+        {
+            urlHelperFactory = helperfactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public PagingInfo pageModel { get; set; }
+        public string PageActrion { get; set; }
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder result = new TagBuilder("div");
+
+            for (int i = 1; i <= pageModel.TotalPage; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                string url = pageModel.UrlParam.Replace(":", i.ToString());
+                tag.Attributes["href"] = url;
+                tag.AddCssClass(PageClass);
+                tag.AddCssClass(i == pageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+    }
+}
