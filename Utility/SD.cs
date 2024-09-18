1eabef1bcf13ee446ce010a6f502b22f5a25cf15
Sandra Borowik <sandra.borowik@vp.pl>
2024-09-18 12:10:06 +0200

    Utility

---
+++ Utility/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AquaGuide2.Utility
+{
+    public static class SD
+    {
+        public const string AdminEndUser = "Admin";
+        public const string PersonEndUser = "User";
+        public const int PaginationUsersPage = 5;
+    }
+}
